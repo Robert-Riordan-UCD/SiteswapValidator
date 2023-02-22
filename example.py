851c7bc52bd2457c01b36953964e545aa16647f8
Robert-Riordan <robert.riordan@ucdconnect.ie>
2023-02-22 23:39:54 +0000

    Made the example clearer

--- example.py
+++ example.py
@@ -6,5 +6,7 @@ validator = Validator()
 
 to_validate = ['3', '441', '531', 'a0', 'TEST', '!£*&', '45', '321']
 
+print("Siteswap | Valid")
+print("---------|------")
 for siteswap in to_validate:
-	print(f"{siteswap:<8} {validator.validate(siteswap)}")
+	print(f"{siteswap:<8} | {validator.validate(siteswap)}")
