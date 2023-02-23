6b6af441597ec836db42a733a4a7695b1fc83993
Robert-Riordan <robert.riordan@ucdconnect.ie>
2023-02-23 18:21:59 +0000

    Added multiplex example

---
+++ multiplex_example.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python3
+
+from siteswap import MultiplexValidator
+
+validator = MultiplexValidator()
+
+to_validate = ['3', '441', '531', 'a0', 'TEST', '!£*&', '45', '321', '[22]0', '[a2]0', '[32][32]', '[32][]', '45', '[75]3', '[32][33]',
+
+print("Siteswap | Valid")
+print("---------|------")
+for siteswap in to_validate:
+	print(f"{siteswap:<8} | {validator.validate(siteswap)}")
+
+validator.print_details('3')
+validator.print_details('441')
+validator.print_details('a0')
+validator.print_details('[22]0')
+validator.print_details('[a2]0')
+validator.print_details('[32][32]')
+validator.print_details('[32][]') # Invalid syntax
+validator.print_details('45') # Invalid number of balls
+validator.print_details('[75]3') # Invalid number of balls
+validator.print_details('[32][33]') # Invalid [xx][xx] -> [xxx][x]
